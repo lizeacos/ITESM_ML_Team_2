bbdde4cdd8497953db73008ef98db3064f021df0
Liz Acosta <lizeacos@gmail.com>
2025-07-04 21:41:35 -0600

    se agrega resta, y corrige nombre de funcion en raiz cuadrada

---
+++ src/calculadora/Operaciones/raiz_cuadrada.py
@@ -0,0 +1,14 @@
+import math
+
+def raiz_cuadrada(numero):
+    """
+    Calcula la raíz cuadrada del número introducido
+
+    Parámetros:
+    *numero (float): Números a calcular.
+
+    Retorna:
+    float: Raíz cuadrada del número introducido.
+    """
+
+    return math.sqrt(numero)
---
+++ src/calculadora/Operaciones/restar.py
@@ -0,0 +1,13 @@
+import operator
+
+def restar(num_a, num_b):
+    """
+    Resta dos números.
+
+    Parámetros:
+    *numeros (float): Números a restar.
+
+    Retorna:
+    float: La resta de los números ingresados.
+    """
+    return num_a - num_b
